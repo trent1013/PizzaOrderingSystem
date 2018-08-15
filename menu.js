e76a46f1703bb5c026a434825a121eb877f7fd28
daniel t taylor <d.trent.taylor@gmail.com>
2018-08-15 12:05:10 -0700

    added cartObj for POST method

---
+++ menu.js
@@ -0,0 +1,15 @@
+var menu = {
+	"soda" : "$1.99",
+    "byo_pizza" : "$14.99",
+    "pepperoni" : "$2.99",
+    "sausage" : "$2.99",
+    "mushrooms" : "$1.99",
+    "peppers" : "$1.99",
+    "pesto" : "$3.99",
+    "breadsticks" : "$4.99",
+    "salad" : "$6.99"
+};
+
+
+
+module.exports = menu;
